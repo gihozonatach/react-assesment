b364d257e164650410f3ffb394a94f0581f3983c
gihozonatach <gihozonatach713@gmail.com>
2026-04-14 00:55:50 -0700

    NatashaGIHOZO

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Modules from "./Modules";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+
+      
+        <nav className="bg-blue-600 text-white flex items-center justify-between px-6 py-4 shadow-md">
+          
+
+          <div className="space-x-6">
+            <Link className="hover:text-yellow-300" to="/">
+              Home
+            </Link>
+
+            <Link className="hover:text-yellow-300" to="/modules">
+              Modules
+            </Link>
+          </div>
+        </nav>
+
+       
+        <div className="p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/modules" element={<Modules />} />
+          </Routes>
+        </div>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Modules.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Modules() {
+  return (
+    <center>
+    <div>
+      <h1>Modules Page</h1>
+      <ul>
+        <li>machine learning</li>
+        <li>devOps</li>
+        <li>python programing</li>
+        <li>backend design</li>
+      </ul>
+    </div></center>
+  );
+}
+
+export default Modules;
